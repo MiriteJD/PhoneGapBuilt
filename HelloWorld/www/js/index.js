d711a3960d3a811af3212ba455aa5151be4bd72b
MiriteJD <i14004@hb.dhbw-stuttgart.de>
2016-12-12 16:55:29 +0100

    new hello world

--- HelloWorld/www/js/index.js
+++ HelloWorld/www/js/index.js
@@ -47,26 +47,3 @@ var app = {
         console.log('Received Event: ' + id);
     }
 };
-//on Success Callback
-//this method excepts a Position object, which contains the//current GPS coordinates
-//
-var onSuccess = function(position){
-	alert('Latitude: '		+ position.coords.latitude	+'\n'+
-	'Latitude: '			+ position.coords.latitude	+'\n'+
-	'Longitude: '			+ position.coords.longitude	+'\n'+
-	'Altitude: '			+ position.coords.altitude	+'\n'+
-	'Accuracy: '			+ position.coords.accuracy	+'\n'+
-	'Altitude Accuracy: '	+ position.coords.altitudeAccuracy	+'\n'+
-	'Heading: '				+ position.coords.heading	+'\n'+
-	'Speed: '				+ position.coords.speed	+'\n'+
-	'Timestamp: '			+ position.coords.timestamp	+'\n');
-};
-//on error Callback receives a PositionError object
-//
-function onError(error){
-	alert('code: '		+ error.code+'\n'+
-	alert('message: '	+ error.message+'\n')
-}
-
-navigator.geolocation.getCurrentPosition(onSuccess,onError);
-
